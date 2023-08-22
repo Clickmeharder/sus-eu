e0a2c3abbc0cb12df69cf828b0c12725d656d1f0
Clickmeharder <69931387+Clickmeharder@users.noreply.github.com>
2023-08-22 15:26:25 -0600

    Add files via upload

---
+++ sus-snake/sus-snake.js
@@ -0,0 +1,261 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById("gameCanvas");
+    const context = canvas.getContext("2d");
+
+    const gridSize = 4;
+    let score = 0;
+    let snake = [{ x: 5, y: 5 }];
+    let food = { x: 10, y: 10 };
+    let direction = "right";
+	let highscores = JSON.parse(localStorage.getItem("highscores")) || [
+		{ name: "Player1", score: 10 },
+		{ name: "Player2", score: 9 },
+		{ name: "Player1", score: 8 },
+		{ name: "Player2", score: 7 },
+		{ name: "Player1", score: 6 },
+		{ name: "Player2", score: 5 },
+
+    ];
+    let isShiftPressed = false;
+    let gameInterval = 150; // Base interval for normal speed
+    const resetButton = document.getElementById("resetButton");
+    resetButton.addEventListener("click", function() {
+        // Change the location to sus-snake.html
+        window.location.href = "sus-snake.html";
+    });
+
+    function drawSnakePart(part, isFast) {
+        context.fillStyle = isFast ? "blue" : "grey"; // Change color for fast speed
+        context.fillRect(
+            part.x * gridSize,
+            part.y * gridSize,
+            gridSize,
+            gridSize
+        );
+
+        // Draw a border around the grid element
+        context.strokeStyle = "black";
+        context.lineWidth = 1;
+        context.strokeRect(
+            part.x * gridSize,
+            part.y * gridSize,
+            gridSize,
+            gridSize
+        );
+    }
+
+    function drawFood() {
+        context.fillStyle = "red";
+        context.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
+    }
+
+    function updateGameArea() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Move the snake
+        const newHead = { x: snake[0].x, y: snake[0].y };
+        switch (direction) {
+            case "up":
+                newHead.y--;
+                break;
+            case "down":
+                newHead.y++;
+                break;
+            case "left":
+                newHead.x--;
+                break;
+            case "right":
+                newHead.x++;
+                break;
+        }
+        // Increment score for moving
+        score += isShiftPressed ? 2 : 1;
+        // Check for collision with food
+        if (newHead.x === food.x && newHead.y === food.y) {
+            snake.push({});
+            generateFood();
+            score += isShiftPressed ? 10 : 5; // Increment score for eating food
+
+            // Update the score display
+            const scoreDisplay = document.getElementById("scoreDisplay");
+            scoreDisplay.textContent = `Score: ${score}`;
+        }
+
+        // Check for collision with walls or self
+        if (
+            newHead.x < 0 ||
+            newHead.y < 0 ||
+            newHead.x >= canvas.width / gridSize ||
+            newHead.y >= canvas.height / gridSize ||
+            snake.some(part => part.x === newHead.x && part.y === newHead.y)
+        ) {
+            clearInterval(gameIntervalId);
+            return;
+        }
+
+        // Move the tail
+        for (let i = snake.length - 1; i > 0; i--) {
+            snake[i] = { ...snake[i - 1] };
+        }
+        snake[0] = newHead;
+
+        // Draw game elements
+        snake.forEach(part => drawSnakePart(part, isShiftPressed)); // Pass the isShiftPressed value
+        drawFood();
+    }
+
+    function generateFood() {
+        food = {
+            x: Math.floor(Math.random() * (canvas.width / gridSize)),
+            y: Math.floor(Math.random() * (canvas.height / gridSize)),
+        };
+    }
+
+    const speedDisplay = document.createElement("div");
+    speedDisplay.style.marginTop = "5px";
+    speedDisplay.style.fontFamily = "Arial, sans-serif";
+    speedDisplay.style.fontSize = "14px";
+    speedDisplay.style.color = "white";
+
+    const speedGaugeContainer = document.createElement("div");
+    speedGaugeContainer.classList.add("speed-gauge-container");
+
+    speedGaugeContainer.appendChild(speedIndicator);
+    speedGaugeContainer.appendChild(speedDisplay);
+
+    document.body.appendChild(speedGaugeContainer);
+
+    // Append speedIndicator and speedDisplay to displayhud-container
+    const displayHudContainer = document.querySelector(".displayhud-container");
+    displayHudContainer.appendChild(speedIndicator);
+    displayHudContainer.appendChild(speedDisplay);
+
+    document.addEventListener("keydown", function(event) {
+        switch (event.key) {
+            case "ArrowUp":
+            case "w":
+            case "W":
+                direction = "up";
+                break;
+            case "ArrowDown":
+            case "s":
+            case "S":
+                direction = "down";
+                break;
+            case "ArrowLeft":
+            case "a":
+            case "A":
+                direction = "left";
+                break;
+            case "ArrowRight":
+            case "d":
+            case "D":
+                direction = "right";
+                break;
+            case "Shift":
+                isShiftPressed = true;
+                gameInterval = isShiftPressed ? 50 : 150; // Adjust the interval for faster speed
+                speedIndicator.style.backgroundColor = "green";
+                speedDisplay.textContent = "Speed: Fast";
+                clearInterval(gameIntervalId);
+                gameIntervalId = setInterval(updateGameArea, gameInterval);
+                gameInterval = isShiftPressed ? baseInterval / 2 : baseInterval;
+                updateSpeedGauge();
+                break;
+        }
+    });
+
+	const saveScoreButton = document.getElementById("savescoreButton");
+
+	saveScoreButton.addEventListener("click", function () {
+		// Prompt the player for their name
+		const playerName = prompt("Congratulations! You've achieved a high score. Please enter your name:");
+
+		if (playerName) {
+			// Check if the score qualifies for the highscores list
+			const lowestHighscore = highscores[highscores.length - 1];
+			if (score > lowestHighscore.score) {
+				// Update the lowest highscore with the new score and name
+				lowestHighscore.name = playerName;
+				lowestHighscore.score = score;
+
+				// Sort and save highscores to localStorage
+				highscores.sort((a, b) => b.score - a.score);
+				localStorage.setItem("highscores", JSON.stringify(highscores));
+
+
+				// Call displayHighscores to refresh the displayed high scores
+				displayHighscores();
+			} else {
+				alert("Your score is not high enough to make it to the top 10 highscores.");
+			}
+		}
+	});
+
+	document.addEventListener("keyup", function(event) {
+        if (event.key === "Shift") {
+            isShiftPressed = false;
+            gameInterval = 150; // Reset interval to normal speed
+            speedIndicator.style.backgroundColor = "red";
+            speedDisplay.textContent = "Speed: Normal";
+            clearInterval(gameIntervalId);
+            gameIntervalId = setInterval(updateGameArea, gameInterval);
+            gameInterval = baseInterval;
+            updateSpeedGauge();
+        }
+    });
+
+    generateFood();
+    let gameIntervalId = setInterval(updateGameArea, gameInterval);
+
+    //----------------------------
+    // Speed gauge -------------
+    //----------------------------
+    //
+    function updateSpeedGauge() {
+        const maxSpeed = 300; // Adjust as needed
+        const normalizedSpeed = gameInterval / maxSpeed;
+        const speedIndicator = document.getElementById("speedIndicator");
+        const speedDisplay = document.getElementById("speedDisplay"); // Make sure to get the speedDisplay element
+
+        speedIndicator.style.width = `${normalizedSpeed * 100}%`;
+        speedIndicator.style.height = "10px"; // Set a fixed height for the speedIndicator
+
+        speedDisplay.textContent = `Speed: ${isShiftPressed ? "Fast" : "Normal"}`;
+    }
+
+    //=======================
+    //-----------------------
+    //       HIGHSCORES
+    //-----------------------
+    //=======================
+
+    function displayHighscores() {
+        const highscoresList = document.getElementById('highscores-list');
+		highscoresList.innerHTML = '';
+
+        highscores.forEach((entry, index) => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${index + 1}. ${entry.name}: ${entry.score}`;
+        highscoresList.appendChild(listItem);
+    });
+}
+
+    // Call displayHighscores to show the highscores on your highscores page
+    displayHighscores();
+});
+
+// When loading the game or resetting highscores
+function resetHighscores() {
+    localStorage.removeItem("highscores");
+    highscores = [
+        { name: "Player1", score: 10 },
+        { name: "Player2", score: 9 },
+        { name: "Player1", score: 8 },
+        { name: "Player2", score: 7 },
+        { name: "Player1", score: 6 },
+        { name: "Player2", score: 5 },
+        // ... Add more highscores here
+    ];
+    displayHighscores();
+}
